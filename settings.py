cdb014ad0c0e9257356991dc01ed913ce0fd91bb
gorillavibe <scrumpcioushats@gmail.com>
2022-12-26 11:24:26 -0700

    game values

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """
+    alien game settings
+    """
+    def __init__(self):
+        """
+        configure settings
+        """
+        self.screen_w = 640
+        self.screen_h = 480
+        self.bgc = (10, 10, 10)
